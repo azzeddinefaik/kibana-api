816961f5e992b786be9590109a80bd5aa19976ea
Anmol Nagpal <ianmolnagpal@gmail.com>
2017-03-21 11:12:55 +0400

    update

--- config/config.php
+++ config/config.php
@@ -4,16 +4,16 @@ return [
     "_id"      => "Apache-HTTPD-CPU",
     "_type"    => "visualization",
     "_source"  => [
-        "title"    => "Apache HTTPD - CPU",
-        "savedSearchId"         => "Apache-HTTPD",
-        "version"  => "1",
         "description"           => " visualisation of CPU",
         "kibanaSavedObjectMeta" =>  [
             "searchSourceJSON" =>json_encode( [
                 "filter" => [],
             ]),
         ] ,
+        "savedSearchId"         => "Apache-HTTPD",
+        "title"    => "Apache HTTPD - CPU",
         "uiStateJSON" => "{}",
+        "version"  => "1",
         "visState" => json_encode( json_decode( file_get_contents( "config/visState.json" ) ) )
     ],
 ];
