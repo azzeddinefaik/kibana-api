cdb1c839cd1fb756a05556c3fbc6a12605de3e5c
Anmol Nagpal <ianmolnagpal@gmail.com>
2017-03-21 17:16:51 +0400

    update

--- dashboard.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$db = require __DIR__. "/dashboard/disk-overview.php";
-file_put_contents("dashboard/db.json",json_encode([$db]));
--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$disk = require __DIR__. "/hotel-api/config.php";
-file_put_contents("hotel-api/output/disk.json",json_encode([$disk]));
