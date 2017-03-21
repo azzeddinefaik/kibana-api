2e3f18b1085d234b684e56de08f1d27a24499b78
Anmol Nagpal <ianmolnagpal@gmail.com>
2017-03-21 14:58:17 +0400

    update

--- common/config.php
+++ common/config.php
@@ -1,34 +1,34 @@
 <?php
 
 return $cpu = [[
-    "_id"     => "Disk space distribution",
+    "_id"     => "Disk space distribution", //variable
     "_type"   => "visualization",
     "_source" => [
-        "description"           => "Disk space distribution",
+        "description"           => "Disk space distribution", //variable
         "kibanaSavedObjectMeta" => [
             "searchSourceJSON" => json_encode([
                 "filter" => [],
             ]),
         ],
-        "savedSearchId"         => "Filesystem-stats",
-        "title"                 => "Disk space distribution",
+        "savedSearchId"         => "Filesystem-stats", //variable
+        "title"                 => "Disk space distribution", //variable
         "uiStateJSON"           => "{}",
         "version"               => "1",
         "visState"              => json_encode(json_decode(file_get_contents(__DIR__ ."/disk/visulisations/disk-space-distribution.json"))),
     ],
 ],
     [
-        "_id"     => "Disk utilization over time",
+        "_id"     => "Disk utilization over time", //variable
         "_type"   => "visualization",
         "_source" => [
-            "description"           => " Disk utilization over time",
+            "description"           => " Disk utilization over time", //variable
             "kibanaSavedObjectMeta" => [
                 "searchSourceJSON" => json_encode([
                     "filter" => [],
                 ]),
             ],
-            "savedSearchId"         => "Filesystem-stats",
-            "title"                 => "Disk utilization over time",
+            "savedSearchId"         => "Filesystem-stats", //variable
+            "title"                 => "Disk utilization over time", //variable
             "uiStateJSON"           => "{}",
             "version"               => "1",
             "visState"              => json_encode(json_decode(file_get_contents(__DIR__."/disk/visulisations/disk-utilization-over-time.json"))),
