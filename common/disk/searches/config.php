4766b6d4b493ad5df7b6f54e4277f44c4d68fab3
Anmol Nagpal <ianmolnagpal@gmail.com>
2017-03-21 17:19:59 +0400

    update

--- common/disk/searches/config.php
+++ common/disk/searches/config.php
@@ -2,22 +2,22 @@
 
 
 return [
-  "_id"=> "$_id",
-    "_type"=> "search",
-    "_source"=> [
-      "columns"=> [
-          "_source"
-      ],
-      "description"=> "",
-      "hits"=> 0,
-      "kibanaSavedObjectMeta"=> [
-          "searchSourceJSON"=> json_encode( json_decode( file_get_contents( __DIR__. "/templates/" . $_id . "-search-source.json" ) ) )
-      ],
-      "sort"=> [
-          "@timestamp",
-          "desc"
-      ],
-      "title" => "$title",
-      "version" => 1
-    ]
+    "_id"     => "$_id",
+    "_type"   => "search",
+    "_source" => [
+        "columns"               => [
+            "_source",
+        ],
+        "description"           => "",
+        "hits"                  => 0,
+        "kibanaSavedObjectMeta" => [
+            "searchSourceJSON" => json_encode(json_decode(file_get_contents(__DIR__ . "/templates/" . $_id . "-search-source.json"))),
+        ],
+        "sort"                  => [
+            "@timestamp",
+            "desc",
+        ],
+        "title"                 => "$title",
+        "version"               => 1,
+    ],
 ];
--- common/disk/visulisations/config.php
+++ common/disk/visulisations/config.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    "_id"      => "$_visId",
-    "_type"    => "visualization",
-    "_source"  => [
+    "_id"     => "$_visId",
+    "_type"   => "visualization",
+    "_source" => [
         "description"           => "$_visId",
-        "kibanaSavedObjectMeta" =>  [
-            "searchSourceJSON" =>json_encode( [
+        "kibanaSavedObjectMeta" => [
+            "searchSourceJSON" => json_encode([
                 "filter" => [],
             ]),
-        ] ,
+        ],
         "savedSearchId"         => "$_id",
-        "title"    => "Disk space overview $_visId",
-        "uiStateJSON" => "{}",
-        "version"  => "1",
-        "visState" => json_encode( json_decode( file_get_contents( __DIR__."/templates/disk-overview-vis.json" ) ) )
+        "title"                 => "Disk space overview $_visId",
+        "uiStateJSON"           => "{}",
+        "version"               => "1",
+        "visState"              => json_encode(json_decode(file_get_contents(__DIR__ . "/templates/disk-overview-vis.json"))),
     ],
 ];
